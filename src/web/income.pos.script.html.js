ea8461cb999850d780063a0a68f825c15dd03776
suyo-haru <suku_eni@outlook.com>
2022-06-07 04:02:03 +0900

    income.pos

--- src/web/income.pos.script.html.js
+++ src/web/income.pos.script.html.js
@@ -7,17 +7,21 @@ return {
         return store.state.income.goodies;
       },
     },
-    setup(props) {
+    setup() {
       const formComp = Vue.ref(null);
       const listRef = Vue.ref(null);
       const currentClerk = Vue.ref(0);
       const goodsItems = Vue.ref([]);
-  
+      const itemCount = Vue.ref([]);
+      const customItems = Vue.ref([]);
+
       return {
         formComp,
         listRef,
         currentClerk,
         goodsItems,
+        customItems,
+        itemCount,
         listEl: Vue.computed(() => listRef.value ? listRef.value.$el : null),
         addDigit(num) {
           if (String(currentClerk.value).length >= 9) {
@@ -39,6 +43,44 @@ return {
         },
         reverseSign() {
           currentClerk.value = currentClerk.value * -1
+        },
+        addItem(item) {
+          if (goodsItems.value.includes(item)) {
+            itemCount.value[goodsItems.value.indexOf(item)] = itemCount.value[goodsItems.value.indexOf(item)] + 1;
+          } else {
+            goodsItems.value.push(item);
+            itemCount.value.push(1);
+          }
+        },
+        removeItem(item) {
+          if (goodsItems.value.includes(item)) {
+            itemCount.value[goodsItems.value.indexOf(item)] += -1;
+            if (itemCount.value[goodsItems.value.indexOf(item)] <= 0) {
+              const i = goodsItems.value.indexOf(item)
+              goodsItems.value.splice(i,1)
+              itemCount.value.splice(i,1)
+            }
+          }
+        },
+        checkCurrentCount(item){
+          if (goodsItems.value.includes(item)) {
+            return itemCount.value[goodsItems.value.indexOf(item)]
+          } else {
+            return -1
+          }
+        },
+        createItem(price) {
+          const itemObject = { name: "その他", price: Number(price)};
+          customItems.value.push(itemObject);
+          goodsItems.value.push(itemObject);
+          itemCount.value.push(1);
+        },
+        total(){
+          let sum = 0;
+          for (const index in goodsItems.value){
+            sum += goodsItems.value[index].price * itemCount.value[index]
+          }
+          return sum
         }
       };
     },
